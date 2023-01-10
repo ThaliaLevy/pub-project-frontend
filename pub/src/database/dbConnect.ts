127af9f32ccf51404d63ca550688ad7a7a979a25
ThaliaLevy <thalialevy@hotmail.com>
2023-01-09 21:57:17 -0300

    att banco

--- pub/src/database/dbConnect.ts
+++ pub/src/database/dbConnect.ts
@@ -6,7 +6,7 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_NAME;
 
-mongoose.connect(`mongodb+srv://thalialevy:gft1@cluster0.txl3lqb.mongodb.net/test`)
+mongoose.connect(`mongodb+srv://thalialevy:123@clustergithub.cngtep6.mongodb.net/test`)
 
 let db = mongoose.connection;
 
